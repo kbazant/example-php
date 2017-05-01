cb7e73316674e034c24aa9c2431c7767066bb5ec
kbazant <krizz100@yahoo.com>
2017-05-01 22:22:59 +0100

    created new page: arrays.php

---
+++ arrays.php
@@ -0,0 +1,29 @@
+<?php
+
+	// Constant
+	define("TITLE", "Variables and Constants ");
+	
+	// Custom Variables
+
+	// PHP Frameworks Array
+
+?>
+
+	<!DOCTYPE html>
+	<html>
+
+	<head>
+		<title>Example PHP Website:
+			<?php echo TITLE; ?>
+		</title>
+	</head>
+
+	<body>
+	
+
+		<!--Copyright info-->
+		<small>&copy;<?php echo $this_year?> - <?php echo $my_name; ?></small>
+
+	</body>
+
+	</html>
