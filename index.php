fbdc29ea1235d2cd9ecbebadfd3f4e2ff7eadb57
kbazant <krizz100@yahoo.com>
2017-05-03 23:49:07 +0100

    added initial structure divs

--- index.php
+++ index.php
@@ -2,13 +2,51 @@
 <html>
 
 <head>
-	<title>Example PHP Website</title>
+	<title></title>
+	<link href="/css/styles.css" rel="stylesheet">
 </head>
 
-<body>
-	<?php
-		print ("Hello World!");
-	?>
+<body id="final-example">
+
+	<div id="wrapper">
+		<div id="banner">
+
+		</div>
+		<!--banner-->
+
+		<div id="nav">
+
+		</div>
+		<!--nav-->
+
+		<div class="content">
+
+			<div id="footer" class="cf">
+
+				<div class="column three">
+
+				</div>
+				<!--column-->
+
+				<div class="column three">
+
+				</div>
+				<!--column-->
+
+				<div class="column three last">
+
+				</div>
+				<!--column-->
+
+			</div>
+			<!--footer-->
+
+		</div>
+		<!--content-->
+
+	</div>
+	<!--wrapper-->
+
 </body>
 
 </html>
