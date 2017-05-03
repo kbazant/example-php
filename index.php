25aabe66bff8fccaba2478169bc0a475254dc652
kbazant <krizz100@yahoo.com>
2017-05-04 00:18:58 +0100

    added main navigation menu links

--- index.php
+++ index.php
@@ -17,7 +17,12 @@
 		<!--banner-->
 
 		<div id="nav">
-
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="menu.php">Menu</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
 		</div>
 		<!--nav-->
 
