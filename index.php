eb7ac7d18aae898de8865633d6225762f63aea85
kbazant <krizz100@yahoo.com>
2017-05-04 00:04:45 +0100

    added contact information and opening hours to 'column' divs

--- index.php
+++ index.php
@@ -24,17 +24,26 @@
 			<div id="footer" class="cf">
 
 				<div class="column three">
-
+					<strong>Phone</strong> 0208 000 0000
 				</div>
 				<!--column-->
 
 				<div class="column three">
-
+					<strong>Location</strong> 1 Street<br> Town, Post Code
 				</div>
 				<!--column-->
 
 				<div class="column three last">
-
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					1pm - 9pm<br><br>
+					
+					<em>Friday - Saturday</em><br>
+					4pm - 11pm<br><br>
+					
+					<em>Sunday - Monday</em><br>
+					Closed<br>
+					
 				</div>
 				<!--column-->
 
