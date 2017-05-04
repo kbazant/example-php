beaf5b6f3e4d0d23e28cbacdc4989d705ce73e90
kbazant <krizz100@yahoo.com>
2017-05-05 00:26:40 +0100

    replaced menu list items with nav.php script

--- includes/header.php
+++ includes/header.php
@@ -18,10 +18,7 @@
 
 		<div id="nav">
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="team.php">Team</a></li>
-				<li><a href="menu.php">Menu</a></li>
-				<li><a href="contact.php">Contact</a></li>
+				 <?php include('includes/nav.php'); ?>
 			</ul>
 		</div>
 		<!--nav-->
