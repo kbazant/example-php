314d0daf61505b87e3d9c036a55a79e1eef58fea
kbazant <krizz100@yahoo.com>
2017-05-04 01:47:45 +0100

    added header section to new file - header.php

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+	<link href="/css/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+
+	<div id="wrapper">
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="http://placehold.it/800x337" alt="Banner Photo">
+			</a>
+		</div>
+		<!--banner-->
+
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="menu.php">Menu</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+		<!--nav-->
+
+		<div class="content">
