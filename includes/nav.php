9929387336a8de77cc27edb5d71ff512e071a6c9
kbazant <krizz100@yahoo.com>
2017-05-05 00:35:31 +0100

    included list items in unordered list

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,6 @@
-<li><a href="index.php">Home</a></li>
-<li><a href="team.php">Team</a></li>
-<li><a href="menu.php">Menu</a></li>
-<li><a href="contact.php">Contact</a></li>
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="team.php">Team</a></li>
+	<li><a href="menu.php">Menu</a></li>
+	<li><a href="contact.php">Contact</a></li>
+</ul>
