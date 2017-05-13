fa15b91075c234fbc29344f433a147397a14dc2d
kbazant <krizz100@yahoo.com>
2017-05-13 01:10:19 +0100

    commented out static opening hours

--- includes/footer.php
+++ includes/footer.php
@@ -1,41 +1,43 @@
-			<div id="footer" class="cf">
+<div id="footer" class="cf">
 
-				<div class="column three">
-					<strong>Phone</strong> 0208 000 0000
-				</div>
-				<!--column-->
+	<div class="column three">
+		<strong>Phone</strong> 0208 000 0000
+	</div>
+	<!--column-->
 
-				<div class="column three">
-					<strong>Location</strong> 1 Street<br> Town, Post Code
-				</div>
-				<!--column-->
+	<div class="column three">
+		<strong>Location</strong> 1 Street<br> Town, Post Code
+	</div>
+	<!--column-->
 
-				<div class="column three last">
-					<strong>Hours</strong>
+	<div class="column three last">
+		<!-- <strong>Hours</strong>
 					<em>Tuesday - Thursday</em><br>
 					1pm - 9pm<br><br>
-					
+
 					<em>Friday - Saturday</em><br>
 					4pm - 11pm<br><br>
-					
+
 					<em>Sunday - Monday</em><br>
-					Closed<br>
-					
-					<?php include('includes/store-hours.php'); ?>
-					
-				</div>
-				<!--column-->
+					Closed<br> -->
 
-			</div>
-			<!--footer-->
-			
-			<small>&copy;<?php echo date('Y'); ?> <?php echo $companyName; ?></small>
-
-		</div>
-		<!--content-->
+		<?php
+                    include('includes/store-hours.php');
+                    ?>
 
 	</div>
-	<!--wrapper-->
+	<!--column-->
+
+</div>
+<!--footer-->
+
+<small>&copy;<?php echo date('Y'); ?> <?php echo $companyName; ?></small>
+
+</div>
+<!--content-->
+
+</div>
+<!--wrapper-->
 
 </body>
 
