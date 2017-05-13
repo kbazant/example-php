86e9d70ed703a4947732a531f2f837be6a421dfd
kbazant <krizz100@yahoo.com>
2017-05-13 01:33:29 +0100

    set all opening hours including exception days

--- includes/store-hours.php
+++ includes/store-hours.php
@@ -1,83 +1,70 @@
- <?php
+<?php
 
-  // REQUIRED
-    // Set your default time zone (listed here: http://php.net/manual/en/timezones.php)
-    date_default_timezone_set('Europe/London');
-    // Include the store hours class
-    require __DIR__ . '/StoreHours.class.php';
+// REQUIRED
+// Set your default time zone (listed here: http://php.net/manual/en/timezones.php)
+date_default_timezone_set('Europe/London');
+// Include the store hours class
+require __DIR__ . '/StoreHours.class.php';
 
-    // REQUIRED
-    // Define daily open hours
-    // Must be in 24-hour format, separated by dash
-    // If closed for the day, leave blank (ex. sunday)
-    // If open multiple times in one day, enter time ranges separated by a comma
+// REQUIRED
+// Define daily open hours
+// Must be in 24-hour format, separated by dash
+// If closed for the day, leave blank (ex. sunday)
+// If open multiple times in one day, enter time ranges separated by a comma
+$hours = array(
+'mon' => array(), //Closed all day
+'tue' => array('13:00-21:00'),
+'wed' => array('13:00-21:00'),
+'thu' => array('13:00-21:00'), 
+'fri' => array('16:00-23:00'),
+'sat' => array('16:00-23:00'),
+'sun' => array()
+);
 
-    // ORIGINAL ARRAY
-    // $hours = array(
-    //     'mon' => array('11:00-20:30'),
-    //     'tue' => array('11:00-13:00', '18:00-20:30'),
-    //     'wed' => array('11:00-13:00', '18:00-20:30'),
-    //     'thu' => array('11:00-1:30'), // Open late
-    //     'fri' => array('11:00-20:30'),
-    //     'sat' => array('11:00-20:00'),
-    //     'sun' => array('11:00-20:00')
-    // );
+// OPTIONAL
+// Add exceptions (great for holidays etc.)
+// MUST be in a format month/day[/year] or [year-]month-day
+// Do not include the year if the exception repeats annually
+$exceptions = array(
+'12/25'  => array(),
+'1/1' => array()
+);
 
-    //NEW ARRAY
-    $hours = array(
-        'mon' => array(), //closed all day
-        'tue' => array('13:00-21:00'),
-        'wed' => array('13:00-21:00'),
-        'thu' => array('13:00-21:00'),
-        'fri' => array('16:00-23:00'),
-        'sat' => array('16:00-23:00'),
-        'sun' => array('16:00-23:00')
-    );
+$config = array(
+'separator'      => ' - ',
+'join'           => ' and ',
+'format'         => 'g:ia',
+'overview_weekdays'  => array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+);
 
-    // OPTIONAL
-    // Add exceptions (great for holidays etc.)
-    // MUST be in a format month/day[/year] or [year-]month-day
-    // Do not include the year if the exception repeats annually
-    $exceptions = array(
-        '2/24'  => array('11:00-18:00'),
-        '10/18' => array('11:00-16:00', '18:00-20:30')
-    );
+// Instantiate class
+$store_hours = new StoreHours($hours, $exceptions, $config);
 
-    $config = array(
-        'separator'      => ' - ',
-        'join'           => ' and ',
-        'format'         => 'g:ia',
-        'overview_weekdays'  => array('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-    );
+// Display open / closed message
+if ($store_hours->is_open()) {
+    echo "Yes, we're open! Today's hours are " . $store_hours->hours_today() . ".";
+} else {
+    echo "Sorry, we're closed. Today's hours are " . $store_hours->hours_today() . ".";
+}
 
-    // Instantiate class
-    $store_hours = new StoreHours($hours, $exceptions, $config);
+// Display full list of open hours (for a week without exceptions)
+echo '<table>';
+foreach ($store_hours->hours_this_week() as $days => $hours) {
+    echo '<tr>';
+    echo '<td>' . $days . '</td>';
+    echo '<td>' . $hours . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
 
-    // Display open / closed message
-    if($store_hours->is_open()) {
-        echo "Yes, we're open! Today's hours are " . $store_hours->hours_today() . ".";
-    } else {
-        echo "Sorry, we're closed. Today's hours are " . $store_hours->hours_today() . ".";
-    }
+// Same list, but group days with identical hours
+echo '<table>';
+foreach ($store_hours->hours_this_week(true) as $days => $hours) {
+    echo '<tr>';
+    echo '<td>' . $days . '</td>';
+    echo '<td>' . $hours . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
 
-    // Display full list of open hours (for a week without exceptions)
-    echo '<table>';
-    foreach ($store_hours->hours_this_week() as $days => $hours) {
-        echo '<tr>';
-        echo '<td>' . $days . '</td>';
-        echo '<td>' . $hours . '</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    // Same list, but group days with identical hours
-    echo '<table>';
-    foreach ($store_hours->hours_this_week(true) as $days => $hours) {
-        echo '<tr>';
-        echo '<td>' . $days . '</td>';
-        echo '<td>' . $hours . '</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    ?>
+?>
