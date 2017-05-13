5389892f84ff691563292595ea1736a77f43d8af
kbazant <krizz100@yahoo.com>
2017-05-13 03:25:58 +0100

    added page description

--- team.php
+++ team.php
@@ -1,5 +1,20 @@
 <?php
     define("TITLE", "Team | Fine Dining Restaurant");
     include('includes/header.php');
+?>
 
+    <div id="team-members" class="cf">
+
+        <h1>Our Team</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta quidem molestias maxime, impedit nam dicta, autem
+            deserunt earum doloremque non blanditiis optio sequi aliquid est nihil perspiciatis praesentium voluptatibus
+            sit.</p>
+
+        <hr>
+
+    </div>
+    <!-- team members -->
+
+    <?php
+    include('includes/footer.php');
 ?>
