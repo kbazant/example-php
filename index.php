4c0932455bdfecf6b5c03f1b292e843989c0731a
kbazant <krizz100@yahoo.com>
2017-05-13 02:29:33 +0100

    added "philosophy" div and 2 paragraphs

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 	include('includes/header.php');
 ?>
 
+<div id="philosophy">
+	<hr>
+	
+		<h1>Philosophy of Fine Dining</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste molestias ipsa tenetur cum cupiditate eligendi culpa id. Ab quo, magni! Aperiam in, eius dignissimos suscipit modi accusantium enim necessitatibus qui.</p>
+		
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+	
+	<hr>
+</div><!--philosophy-->
+
 
 <?php 
 	include('includes/footer.php');
