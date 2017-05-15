eaaec11e1c1b1347b598926540312bf4c3315ba1
kbazant <krizz100@yahoo.com>
2017-05-15 02:12:06 +0100

    Added preg_replace (to perform a regular expression search and replace)

--- dish.php
+++ dish.php
@@ -1,6 +1,12 @@
 <?php
     define("TITLE", "Menu Item | Fine Dining Restaurant");
     include('includes/header.php');
+
+    function strip_bad_chars($input) {
+        $output = preg_replace("/[^a-zA-Z0-9_-]", "", $input);
+        return $output;
+    }
+
     if (isset($_GET['item'])) {
         $menuItem = $_GET['item'];
         $dish = $menuIems[$menuItem];
