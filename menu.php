6fc5f424ddab1f794240e40e4d6d716c42b6050c
kbazant <krizz100@yahoo.com>
2017-05-15 01:33:32 +0100

    added "menu-items" div paragraphs and menu items links

--- menu.php
+++ menu.php
@@ -3,6 +3,24 @@
     include('includes/header.php');
 ?>
 
-<?php
-    include('includes/footer.php');
-?>
+    <div id="menu-items">
+
+        <h1>Our Delicious Menu</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde magni temporibus, quam iusto sequi harum architecto maxime facilis illum aperiam perspiciatis vero atque ratione, aliquam, voluptatibus aut dicta assumenda quos.</p>
+        <p><em>Cick any menu item to learn more about it.</em></p>
+
+        <hr>
+
+        <ul>
+            <?php foreach ($menuItems as $dish => $item) { ?>
+
+                <li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item[title]; ?> </a><sup>$</sup><?php echo $item[price]; ?></li>
+
+                <?php } ?>
+
+        </ul>
+
+    </div>
+    <!-- menu-items -->
+
+    <?php include('includes/footer.php');
